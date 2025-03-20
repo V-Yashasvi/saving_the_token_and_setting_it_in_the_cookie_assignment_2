9047b725d52135760ea2437e5ea93ec5843172c8
V-Yashasvi <vyashasvi1@gmail.com>
2025-03-20 10:48:53 +0530

    completed the assignment

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 const jwt=require('jsonwebtoken')
 require('dotenv').config()
+const secret=process.env.JWT_SECRET
 
-const encrypt = (payload, secret=process.env.JWT_SECRET) => {
+const encrypt = (payload, secret) => {
   // your code here and return token
   return jwt.sign(payload, secret, {expiresIn:'1hr', algorithm:'HS256'})
 };
 
 const payload={userId:"1", username:"abc"}
-const token=encrypt(payload)
+const token=encrypt(payload, secret)
 console.log(token)
 
 module.exports = encrypt;
